ef3c698e20629fd9f512380370a959c1f38c6576
Peter <peterishumi@gmail.com>
2025-08-05 15:05:21 +0300

    Initial commit

---
+++ components/Footer.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { assets } from "@/assets/assets";
+import Image from "next/image";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="flex flex-col md:flex-row items-start justify-center px-6 md:px-16 lg:px-32 gap-10 py-14 border-b border-gray-500/30 text-gray-500">
+        <div className="w-4/5">
+          <Image className="w-28 md:w-32" src={assets.logo} alt="logo" />
+          <p className="mt-6 text-sm">
+            We are a consumer electronics provider that connects people to reliable, 
+            Certified Pre-Owned Devices. We make it affordable and easy for you to get 
+            the most out of your personal technology purchases.
+          </p>
+        </div>
+
+        <div className="w-1/2 flex items-center justify-start md:justify-center">
+          <div>
+            <h2 className="font-medium text-gray-900 mb-5">Company</h2>
+            <ul className="text-sm space-y-2">
+              <li>
+                <a className="hover:underline transition" href="#">Home</a>
+              </li>
+              <li>
+                <a className="hover:underline transition" href="#">About us</a>
+              </li>
+              <li>
+                <a className="hover:underline transition" href="#">Contact us</a>
+              </li>
+              <li>
+                <a className="hover:underline transition" href="#">Privacy policy</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="w-1/2 flex items-start justify-start md:justify-center">
+          <div>
+            <h2 className="font-medium text-gray-900 mb-5">Get in touch</h2>
+            <div className="text-sm space-y-2">
+              <p>+255-616-824-508</p>
+              <p>contact@petergodwin1@icloud.com</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className="py-4 px-4 text-start text-xs md:text-sm">
+        Copyright 2025 © PeterGee.dev All Right Reserved.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ components/Navbar.jsx
@@ -0,0 +1,125 @@
+"use client";
+import React from "react";
+import { assets, BagIcon, BoxIcon, HomeIcon } from "@/assets/assets";
+import Link from "next/link";
+import { useAppContext } from "@/context/AppContext";
+import Image from "next/image";
+import { useClerk, UserButton } from "@clerk/nextjs";
+
+const Navbar = () => {
+  const { isSeller, router, user } = useAppContext();
+  const { openSignIn } = useClerk();
+
+  return (
+    <nav className="fixed top-0 left-0 w-full z-50 bg-white flex items-center justify-between px-8 md:px-16 lg:px-26 py-3 border border-gray-300 text-gray-700">
+      <Image
+        className="cursor-pointer w-28 md:w-32"
+        onClick={() => router.push("/")}
+        src={assets.logo}
+        alt="logo"
+      />
+      <div className="flex items-center gap-4 lg:gap-8 max-md:hidden">
+        <Link href="/" className="hover:text-gray-900 transition">
+          Home
+        </Link>
+        <Link href="/all-products" className="hover:text-gray-900 transition">
+          Shop
+        </Link>
+        <Link href="/" className="hover:text-gray-900 transition">
+          About Us
+        </Link>
+        <Link href="/" className="hover:text-gray-900 transition">
+          Contact
+        </Link>
+
+        {isSeller && (
+          <button
+            onClick={() => router.push("/seller")}
+            className="text-xs border px-4 py-1.5 rounded"
+          >
+            Seller Dashboard
+          </button>
+        )}
+      </div>
+
+      <ul className="hidden md:flex items-center gap-4 ">
+        <Image className="w-4 h-4" src={assets.search_icon} alt="search icon" />
+        {user ? (
+          <>
+            <UserButton>
+              <UserButton.MenuItems>
+                <UserButton.Action
+                  label="Home"
+                  labelIcon={<HomeIcon />}
+                  onClick={() => router.push("/")}
+                />
+              </UserButton.MenuItems>
+              <UserButton.MenuItems>
+                <UserButton.Action
+                  label="Products"
+                  labelIcon={<BoxIcon />}
+                  onClick={() => router.push("all-products")}
+                />
+              </UserButton.MenuItems>
+              <UserButton.MenuItems>
+                <UserButton.Action
+                  label="My orders"
+                  labelIcon={<BagIcon />}
+                  onClick={() => router.push("my-orders")}
+                />
+              </UserButton.MenuItems>
+            </UserButton>
+          </>
+        ) : (
+          <button
+            onClick={openSignIn}
+            className="flex items-center gap-2 hover:text-gray-900 transition"
+          >
+            <Image src={assets.user_icon} alt="user icon" />
+            Account
+          </button>
+        )}
+        <button onClick={() => router.push("/cart")}>
+          <Image src={assets.cart_icon} alt="cart icon" />
+        </button>
+      </ul>
+
+      <div className="flex items-center md:hidden gap-4 sm:gap-6 w-full justify-between">
+        {isSeller && (
+          <button
+            onClick={() => router.push("/seller")}
+            className="text-xs border px-4 py-1.5 rounded-full"
+          >
+            Seller Dashboard
+          </button>
+        )}
+        {user ? (
+          <>
+            <UserButton>
+              <UserButton.MenuItems>
+                <UserButton.Action
+                  label="My orders"
+                  labelIcon={<BagIcon />}
+                  onClick={() => router.push("my-orders")}
+                />
+              </UserButton.MenuItems>
+            </UserButton>
+          </>
+        ) : (
+          <button
+            onClick={openSignIn}
+            className="flex items-center gap-2 hover:text-gray-900 transition"
+          >
+            <Image src={assets.user_icon} alt="user icon" />
+            Account
+          </button>
+        )}
+        <button onClick={() => router.push("/cart")}>
+          <Image src={assets.cart_icon} alt="cart icon" />
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
