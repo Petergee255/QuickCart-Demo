301ba0f416b24fb7299366547bf395a2dc8f12b1
Peter <peterishumi@gmail.com>
2025-08-07 10:58:05 +0300

    commit

--- config/inngest.js
+++ config/inngest.js
@@ -10,19 +10,19 @@ export const syncUserCreation = inngest.createFunction(
   { id: "sync-user-from-clerk" },
   { event: "clerk/user.created" },
   async ({ event }) => {
-    const { id, first_name, last_name, email_addresses, image_url } =
-      event.data;
+    const { id, first_name, last_name, email_addresses, image_url } = event.data;
+
+    const email = email_addresses?.[0]?.email_address || "";
 
     const userData = {
       _id: id,
-      email: email_addresses[0].email_address,
-      name: first_name +''+ last_name,
+      email,
+      name: `${first_name} ${last_name}`,
       imageUrl: image_url,
     };
 
     await connectDB();  
     await User.create(userData);
-    
   }
 );
 
